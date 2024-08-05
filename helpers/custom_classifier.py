63d0deceaec80e1f969157d8c1eb2325dde30869
Marek Małek <mmalek06@gmail.com>
2024-08-05 18:39:11 +0200

    building a custom classifier

--- helpers/custom_classifier.py
+++ helpers/custom_classifier.py
@@ -3,24 +3,29 @@ import torch.nn as nn
 
 
 class CustomClassifier(nn.Module):
-    def __init__(self, input_shape=(3, 224, 224)):
+    def __init__(
+            self,
+            input_shape: tuple[int, int, int] = (3, 224, 224),
+            conv_out_shapes: tuple[int, int] = (64, 128),
+            linear_layers_features: int = 512
+    ):
         super().__init__()
 
         self.feature_extractor = nn.Sequential(
-            nn.Conv2d(3, 64, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(3, conv_out_shapes[0], kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2),
-            nn.Conv2d(64, 128, kernel_size=3, stride=1, padding=1),
+            nn.Conv2d(conv_out_shapes[0], conv_out_shapes[1], kernel_size=3, stride=1, padding=1),
             nn.ReLU(inplace=True),
             nn.MaxPool2d(kernel_size=2, stride=2)
         )
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(self._get_feature_size(input_shape), 512),
+            nn.Linear(self._get_feature_size(input_shape), linear_layers_features),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 512),
+            nn.Linear(linear_layers_features, linear_layers_features),
             nn.ReLU(inplace=True),
-            nn.Linear(512, 1)
+            nn.Linear(linear_layers_features, 1)
         )
 
     def _get_feature_size(self, shape):
