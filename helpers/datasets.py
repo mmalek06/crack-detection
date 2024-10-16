86e833bf242ff92c3ef3f9328df8169cef9250ad
Marek Małek <mmalek06@gmail.com>
2024-10-16 19:42:49 +0200

    experimenting with frozen feature extractor

--- helpers/datasets.py
+++ helpers/datasets.py
@@ -27,6 +27,8 @@ class CrackDataset(Dataset):
         self.transform = transforms.Compose([
             transforms.ToPILImage(),
             transforms.Resize((224, 224)),
+            transforms.Normalize(mean=[0.485, 0.456, 0.406],
+                                 std=[0.229, 0.224, 0.225]),
             transforms.ToTensor()
         ])
 
@@ -122,119 +124,6 @@ class CrackDatasetForClassificationWithProposals(Dataset):
             self,
             selective_search_runner: Callable,
             coco_file_path: str,
-            images_dir: str,
-            transform: transforms.Compose,
-            proposal_cache: dict,
-            device: torch.device,
-            batch_size: int = 70
-    ):
-        with open(coco_file_path, "r") as f:
-            self.coco_data = json.load(f)
-
-        self.selective_search_runner = selective_search_runner
-        self.images_dir = images_dir
-        self.image_files = [f for f in os.listdir(images_dir) if os.path.isfile(os.path.join(images_dir, f))]
-        self.transform = transform
-        self.proposal_cache = proposal_cache
-        self.device = device
-        self.batch_size = batch_size
-        self.image_id_map = {image_info["file_name"]: image_info["id"] for image_info in self.coco_data["images"]}
-        self.annotations = self._group_annotations_by_image(self.coco_data["annotations"])
-
-    def __len__(self):
-        return len(self.image_files)
-
-    def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor]:
-        img_name = self.image_files[idx]
-        img_path = os.path.join(self.images_dir, img_name)
-        image = PIL.Image.open(img_path).convert("RGB")
-        image_np = np.array(image)
-        image_id = self.image_id_map[img_name]
-        proposals = []
-
-        for proposal_batch in self.selective_search_runner(image_np, img_path, self.batch_size):
-            proposals.append(proposal_batch)
-
-        proposals = torch.cat(proposals, dim=0).to(self.device)
-        ground_truth_boxes = torch.tensor(self._parse_annotations(self.annotations.get(image_id, [])), dtype=torch.float32).to(self.device)
-        labels = self._label_proposals(proposals, ground_truth_boxes)
-        transformed_proposals = []
-
-        for proposal in proposals:
-            x1, y1, x2, y2 = proposal.int().tolist()
-            cropped_image = image.crop((x1, y1, x2, y2))
-            cropped_image = self.transform(cropped_image)
-            transformed_proposals.append(cropped_image)
-
-        return torch.stack(transformed_proposals), labels.clone().detach()
-
-    @staticmethod
-    def _label_proposals(
-            proposals: torch.Tensor,
-            ground_truth_boxes: torch.Tensor,
-            iou_threshold: float = 0.01
-    ) -> torch.Tensor:
-        if ground_truth_boxes.numel() == 0:
-            return torch.zeros(proposals.size(0))
-
-        ious = torchvision.ops.box_iou(proposals, ground_truth_boxes)
-        labels = (ious.max(dim=1)[0] > iou_threshold).float()
-
-        return labels
-
-    @staticmethod
-    def _group_annotations_by_image(annotations: list[dict]) -> dict[int, list[dict]]:
-        grouped_annotations = {}
-
-        for annotation in annotations:
-            image_id = annotation["image_id"]
-
-            if image_id not in grouped_annotations:
-                grouped_annotations[image_id] = []
-
-            grouped_annotations[image_id].append(annotation)
-
-        return grouped_annotations
-
-    @staticmethod
-    def _parse_annotations(annotations: list[dict]) -> np.ndarray:
-        """
-        Parse a list of annotation dictionaries into an array of bounding boxes.
-
-        Args:
-        - annotations (list[dict]): List of annotation dictionaries with 'bbox' fields.
-
-        Returns:
-        - np.ndarray: Array of bounding boxes in the format [x_min, y_min, x_max, y_max].
-        """
-        bboxes = []
-
-        for annotation in annotations:
-            bbox = annotation.get("bbox", [])
-
-            if not bbox or len(bbox) != 4:
-                continue
-
-            x_min, y_min, width, height = map(int, bbox)
-
-            if width <= 0 or height <= 0:
-                continue
-
-            x_max, y_max = x_min + width, y_min + height
-
-            bboxes.append([x_min, y_min, x_max, y_max])
-
-        if not bboxes:
-            return np.zeros((0, 4), dtype=np.float32)
-
-        return np.array(bboxes, dtype=np.float32)
-
-
-class CrackDataset2(Dataset):
-    def __init__(
-            self,
-            selective_search_runner: Callable,
-            coco_file_path: str,
             images_dir: str
     ):
         with open(coco_file_path, "r") as f:
@@ -245,7 +134,7 @@ class CrackDataset2(Dataset):
         self.image_data = [
             img
             for img in self.coco_data["images"]
-            if CrackDataset2._load_image(os.path.join(self.images_dir, img["file_name"])) is not None
+            if CrackDatasetForClassificationWithProposals._load_image(os.path.join(self.images_dir, img["file_name"])) is not None
         ]
         self.annotations = self._group_annotations_by_image(self.coco_data["annotations"])
         self.transform = transforms.Compose([
@@ -258,25 +147,14 @@ class CrackDataset2(Dataset):
         return len(self.image_data)
 
     def __getitem__(self, idx: int) -> tuple[torch.Tensor, torch.Tensor, torch.Tensor]:
-        """
-        Returns:
-        - image_path: path to the image
-        - image: the image as a numpy array
-        - bboxes: a list of bounding boxes in [x_min, y_min, x_max, y_max] format
-        """
         image_info = self.image_data[idx]
         image_path = os.path.join(self.images_dir, image_info["file_name"])
-        image = CrackDataset2._load_image(image_path)
+        image = CrackDatasetForClassificationWithProposals._load_image(image_path)
         image = self.transform(image)
-        proposals = []
-
-        for batch in self.selective_search_runner(image.permute(1, 2, 0).numpy(), image_path):
-            proposals.extend(batch)
-
+        proposals = self.selective_search_runner(image.permute(1, 2, 0).numpy(), image_path)
         ground_truth_boxes = torch.tensor(
             self._parse_annotations(self.annotations.get(image_info["id"], [])),
             dtype=torch.float32)
-        proposals = torch.stack(proposals, dim=0)
 
         if ground_truth_boxes.numel() == 0:
             return image, proposals, torch.zeros(proposals.size(0))
@@ -333,20 +211,6 @@ class CrackDataset2(Dataset):
         image = cv2.imread(image_path, cv2.IMREAD_COLOR)
 
         return image
-
-    @staticmethod
-    def _label_proposals(
-            proposals: torch.Tensor,
-            ground_truth_boxes: torch.Tensor,
-            iou_threshold: float = 0.01
-    ) -> torch.Tensor:
-        if ground_truth_boxes.numel() == 0:
-            return torch.zeros(proposals.size(0))
-
-        ious = torchvision.ops.box_iou(proposals, ground_truth_boxes)
-        labels = (ious.max(dim=1)[0] > iou_threshold).float()
-
-        return labels
 
 
 def custom_collate_fn(batch) -> tuple[list[str], list[np.ndarray], list[int]]:
